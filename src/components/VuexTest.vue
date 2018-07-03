d4966d8679e192d5d65cc79ea48275389853944b
mukolweke <michaelolukak@gmail.com>
2018-07-03 11:09:56 +0300

    updt

--- src/components/VuexTest.vue
+++ src/components/VuexTest.vue
@@ -1,38 +1,55 @@
 <template>
     <div id="VuexTest">
         <h1>{{title2}}</h1>
-        <p>Students Marks: &nbsp; {{getMarks}}&nbsp;{{getRemark}}</p>
+        <div class="all_view">
+            <div class="div1">
 
-        <button @click="activate">Enter Marks</button>
+                <p>Students Marks: &nbsp; {{getMarks}}&nbsp;{{getRemark}}</p>
 
-        <form>
+                <form>
 
-            Mathematics:<br>
-            <input v-model="items.mathematics" required><br>
+                    Mathematics:<br>
+                    <input v-model="items.mathematics" required><br>
 
-            Biology<br>
-            <input v-model="items.biology" required><br>
+                    Biology<br>
+                    <input v-model="items.biology" required><br>
 
-            English:<br>
-            <input v-model="items.english" required><br>
+                    English:<br>
+                    <input v-model="items.english" required><br>
 
-            Kiswahili:<br>
-            <input v-model="items.kiswahili" required><br>
+                    Kiswahili:<br>
+                    <input v-model="items.kiswahili" required><br>
 
-            Socials:<br>
-            <input v-model="items.socials" required><br>
+                    Socials:<br>
+                    <input v-model="items.socials" required><br>
 
-            Chemistry:<br>
-            <input v-model="items.chemistry" required><br>
+                    Chemistry:<br>
+                    <input v-model="items.chemistry" required><br>
 
-            Physics:<br>
-            <input v-model="items.physics"required><br>
+                    Physics:<br>
+                    <input v-model="items.physics" required><br>
 
-        </form>
+                </form>
 
-        <br>
+                <br>
 
-        <button type="button" :disabled="submitted" @click="pressed">Add Marks&nbsp;{{getMarks}}</button>
+                <button type="button" :disabled="submitted" @click="pressed">Add Marks&nbsp;{{getMarks}}</button>
+            </div>
+
+            <div class="div2">
+
+
+                <h2>Bank</h2>
+                <p>Account Balance: {{getCash}}</p>
+                <form>
+                    <input v-model="money_used" required>
+                    <button @click="getBalance">Get Balance</button>
+                </form>
+                <p>Balance: {{getMoney}}</p>
+
+            </div>
+
+        </div>
 
     </div>
 </template>
@@ -41,56 +58,66 @@
     export default {
         name: "VuexTest",
 
-        data(){
-            return{
-               items:{},
-                activate: false,
-                // submitted: true
+        data() {
+            return {
+                items: {},
+                money_used: null,
+                submitted: false,
+                cash: 10000,
+                getCash: this.$store.state.cash_in_bank,
+                getBal: this.$store.state.money_balance
             }
         },
 
-        computed:{
+        computed: {
 
-            getMarks(){
+            getMarks() {
 
                 return this.$store.getters.getMarks
             },
 
-            title2(){
+            title2() {
 
                 return this.$store.state.title2
 
             },
 
-            count(){
 
-                return this.$store.getters.counter;
-
-            },
-
-            getRemark(){
+            getRemark() {
 
                 return this.$store.getters.getRemark
 
             },
+
+            getMoney() {
+
+                return this.$store.state.money_balance
+
+            }
+
         },
 
-        methods:{
-            pressed(){
+        mounted() {
+
+
+        },
+
+        methods: {
+            pressed() {
 
                 this.$store.commit('addMarks', this.items) //access the mutations direct
                 this.items = {}
-                this.activate = true
                 this.submitted = true
-                this.getRemark()
+                return this.getRemark()
                 // this.$store.dispatch('addMarks', 2)
             },
 
+            getBalance() {
 
+                this.$store.commit('decrement_cash', this.money_used);
+                this.money_used = null
 
-            activate(){
-                this.submitted = false
-            }
+            },
 
 
         }
@@ -98,11 +125,26 @@
 </script>
 
 <style scoped>
-
+    .all_view{
+        width: 700px;
+        margin: 0 auto;
+    }
 
     h1, h2 {
         font-weight: normal;
     }
 
+    .div1 {
 
+        width: 50%;
+        float: left;
+
+    }
+
+    .div2 {
+
+        width: 50%;
+        float: left;
+
+    }
 </style>
